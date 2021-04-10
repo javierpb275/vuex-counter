563e55c6fa7d44763446a20a7518984154a07c70
javierpb275 <javierpb275@gmail.com>
2021-04-10 12:25:10 +0200

    added some comments. export default { name: 'App',} = Vue.component('app', {}). scoped=this styles will only be apllied in this component

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,28 @@
+<!--A component ALWAYS has to have a template-->
 <template>
+<!--<div id="app"> this refers to App.vue-->
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
+    <p>{{ message }}</p>
   </div>
 </template>
 
+<!--script is optional-->
 <script>
-
-
+//App.vue is run by default
 export default {
   name: 'App',
+  data() {
+    return {
+      message: 'Hello World of Vue!'
+    }
+  }
 }
 </script>
 
-<style lang="stylus">
+<!--style is optional-->
+<!--preprocessor of styles chosen is stylus. and here we are refering to #app-->
+<style  lang="stylus" scoped>
 #app
   font-family Avenir, Helvetica, Arial, sans-serif
   -webkit-font-smoothing antialiased
--- src/main.js
+++ src/main.js
@@ -6,5 +6,5 @@ Vue.config.productionTip = false
 
 new Vue({
   store,
-  render: h => h(App)
-}).$mount('#app')
+  render: h => h(App)//we are rendering the component App
+}).$mount('#app')//the element is the one that has id="app"
