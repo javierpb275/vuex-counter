dab71e8d8d8b9e04c22c798ba5b17a59c59c0b70
javierpb275 <javierpb275@gmail.com>
2021-04-10 12:55:26 +0200

    added comments. also added counter to the state and 2 methods to mutations

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+//We are importing store from index.js
 import store from './store'
 
 Vue.config.productionTip = false
 
+//we pass store to the instance of Vue. store = store: store
 new Vue({
   store,
   render: h => h(App)//we are rendering the component App
--- src/store/index.js
+++ src/store/index.js
@@ -2,13 +2,26 @@ import Vue from 'vue'
 //vuex is a stated management pattern + library (like redux)
 import Vuex from 'vuex'
 
+//Vuex is a plugin so we have to use this syntax:
 Vue.use(Vuex)
 
+//In order to create the instance of Vuex:
+//Inside Store we can do whatever we want
 export default new Vuex.Store({
+  //state is the data of our app
   state: {
+    counter: 0
   },
+  //mutations are used to alter the state
   mutations: {
+    increment(state) {
+      state.counter += 1;
+    },
+    decrement(state) {
+      state.counter -= 1;
+    }
   },
+  //actions are used to make asynchronous queries to servers
   actions: {
   },
   modules: {
