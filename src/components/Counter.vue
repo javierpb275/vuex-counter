4ed6cbb657df2a3820af93b6dd4e65f2f9e8fa7e
javierpb275 <javierpb275@gmail.com>
2021-04-10 13:35:18 +0200

    accessing counter from the state of store and the methods from store and using then in the counter component

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -1,6 +1,14 @@
 <template>
     <div class="container">
-        Hello From Counter
+        <button class="btn btn-success btn-block" @click="$store.commit('increment')">
+            Increment
+        </button>
+        <div class="alert alert-secondary text-center mt-3">
+            {{ $store.state.counter }}
+        </div>
+        <button class="btn btn-danger btn-block"  @click="$store.commit('decrement')">
+            Decrement
+        </button>
     </div>
 </template>
 
